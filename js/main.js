f7017980f542997978b313128feb4c10047bf96a
grantal <AlexV348@gmail.com>
2018-05-09 13:36:58 -0700

    move main javascript to new file

---
+++ js/main.js
@@ -0,0 +1,123 @@
+var container;
+
+var camera, scene, renderer;
+
+var uniforms;
+
+var mouseX = 0, mouseY = 0;
+
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+
+init();
+animate();
+
+
+function init() {
+
+    container = document.createElement( "div" );
+    document.body.appendChild( container );
+
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+    camera.position.z = 250;
+
+    // scene
+
+    scene = new THREE.Scene();
+
+    var ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
+    scene.add( ambientLight );
+
+    var pointLight = new THREE.PointLight( 0xffffff, 0.8 );
+    camera.add( pointLight );
+    scene.add( camera );
+
+    // shaders
+
+    uniforms = {
+        color:     { value: new THREE.Color( 0xff2200 ) }
+    };
+
+    var shaderMaterial = new THREE.ShaderMaterial( {
+        uniforms: uniforms,
+        vertexShader:document.getElementById( "vertexshader" ).textContent,
+        fragmentShader: document.getElementById( "fragmentshader" ).textContent
+    });
+
+    // model
+
+    var manager = new THREE.LoadingManager();
+    var onProgress = function ( xhr ) {
+        if ( xhr.lengthComputable ) {
+            var percentComplete = xhr.loaded / xhr.total * 100;
+            console.log( Math.round(percentComplete, 2) + "% downloaded" );
+        }
+    };
+
+    var onError = function ( xhr ) {
+    };
+
+    var loader = new THREE.OBJLoader( manager );
+    loader.load( "models/cube.obj", function ( object ) {
+        // we need to get the geomery of the object3D so we can add our material to it
+        var geometry = object.children[0].geometry;
+        let shadeMesh = new THREE.Mesh(geometry, shaderMaterial);
+        shadeMesh.scale.set(50,50,50);
+        scene.add(shadeMesh);
+    }, onProgress, onError );
+
+    //
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+
+    document.addEventListener( "mousemove", onDocumentMouseMove, false );
+
+    //
+
+    window.addEventListener( "resize", onWindowResize, false );
+
+}
+
+function onWindowResize() {
+
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function onDocumentMouseMove( event ) {
+
+    mouseX = ( event.clientX - windowHalfX ) / 2;
+    mouseY = ( event.clientY - windowHalfY ) / 2;
+
+}
+
+//
+
+function animate() {
+
+    requestAnimationFrame( animate );
+    render();
+
+}
+
+function render() {
+
+    camera.position.x += ( mouseX - camera.position.x ) * .05;
+    camera.position.y += ( - mouseY - camera.position.y ) * .05;
+
+    camera.lookAt( scene.position );
+
+    renderer.render( scene, camera );
+
+}
+
