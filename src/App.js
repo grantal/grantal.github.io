cef827aa35dae7e7db49237dd238ef103985077d
grantal <AlexV348@gmail.com>
2018-09-18 15:50:45 -0700

    made canvas fit properly

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import ThreeScene from './ThreeScene.js';
 class App extends Component {
   render() {
     return (
-      <div className="App">
         <ThreeScene></ThreeScene>
-      </div>
     );
   }
 }
